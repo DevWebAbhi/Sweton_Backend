0d7ace3e1a920463469a522c48915b1ca5451640
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-01-21 08:13:06 +0530

    Backend

--- Backend/mongoDBConnection.js
+++ Backend/mongoDBConnection.js
@@ -4,6 +4,8 @@ const MONGO_URL=process.env.MONGO_URL;
 
 const connect=mongoose.connect(`mongodb+srv://tiwariabhishek889912:mongodb@cluster.62hpvic.mongodb.net/`);
 
+
+
 module.exports={
     connect
 }
